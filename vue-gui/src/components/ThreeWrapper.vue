4b06108a20b43f03789c285f5f3eeca281f63156
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2024-03-17 12:05:44 +0100

    bases for threejs

--- vue-gui/src/components/ThreeWrapper.vue
+++ vue-gui/src/components/ThreeWrapper.vue
@@ -6,26 +6,88 @@
 </template>
 
 <script lang="js">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import * as THREE from 'three'
 import { Dialog, DialogDescription, DialogPanel, DialogTitle } from '@headlessui/vue'
 import NoWebGLDialog from '@/components/util/NoWebGLDialog.vue'
 import WebGL from 'three/addons/capabilities/WebGL.js'
 import { MapControls } from 'three/addons/controls/MapControls.js'
 
-if (WebGL.isWebGLAvailable()) {
-// do not put these in the data() function because they will break if in a proxy
-  var scene = new THREE.Scene()
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  var renderer = new THREE.WebGLRenderer()
+const sceneObjects = []
+const sceneObjectsLen = ref(0)
 
-  var geometry = new THREE.BoxGeometry(1, 1, 1)
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  var cube = new THREE.Mesh(geometry, material)
+function addSceneObject(obj) {
+  scene.add(obj)
+  sceneObjects.push(obj)
+  sceneObjectsLen.value = sceneObjects.length
+}
+
+var scene, camera, renderer, controls
+
+function initWorld() {
+  // do not put these in the data() function because they will break if in a proxy
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.set(0, 5, 5)
+  renderer = new THREE.WebGLRenderer()
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
+  renderer.pixelRatio = window.devicePixelRatio
 
   // controls: see https://threejs.org/docs/#examples/en/controls/MapControls
-  var controls = new MapControls(camera, renderer.domElement)
+  controls = new MapControls(camera, renderer.domElement)
   controls.enableDamping = true
+  controls.minPolarAngle = 0
+  controls.maxPolarAngle = Math.PI / 2 - 0.1
+  controls.minDistance = 2
+  controls.maxDistance = 25
+
+  //background color
+  scene.background = new THREE.Color(0xffe699)
+
+  const ambiantLight = new THREE.AmbientLight(0xaaaaaa) // soft white light
+  addSceneObject(ambiantLight)
+
+  const light = new THREE.DirectionalLight(0xffffff, 1.6)
+  light.position.set(12, 41, 12)
+  light.target.position.set(0, 0, 0)
+  light.castShadow = true
+  light.shadow.bias = 0
+  light.shadow.mapSize.width = 5000
+  light.shadow.mapSize.height = 5000
+  light.shadow.camera.near = 0.1
+  light.shadow.camera.far = 200
+  light.shadow.camera.left = -25
+  light.shadow.camera.right = 25
+  light.shadow.camera.top = 25
+  light.shadow.camera.bottom = -25
+  addSceneObject(light)
+
+  // debug shadow camera
+  // addSceneObject(new THREE.CameraHelper( light.shadow.camera ))
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(50, 50, 1, 1), new THREE.MeshLambertMaterial({ color: 0xffebab }))
+  plane.receiveShadow = true
+  plane.castShadow = false
+  plane.rotation.x = -Math.PI / 2
+  addSceneObject(plane)
+
+  const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshLambertMaterial({ color: 0x00ff00 }))
+  cube.castShadow = true
+  cube.receiveShadow = true
+  addSceneObject(cube)
+
+  const cubeX = new THREE.Mesh(new THREE.BoxGeometry(2, 0.5, 0.5), new THREE.MeshLambertMaterial({ color: 0xff0000 }))
+  cubeX.position.x = 2
+  cubeX.castShadow = true
+  cubeX.receiveShadow = true
+  addSceneObject(cubeX)
+
+  const cubeY = new THREE.Mesh(new THREE.BoxGeometry(0.5, 2, 0.5), new THREE.MeshLambertMaterial({ color: 0x0000ff }))
+  cubeY.position.y = 2
+  cubeY.castShadow = true
+  cubeY.receiveShadow = true
+  addSceneObject(cubeY)
 }
 
 export default defineComponent({
@@ -40,6 +102,8 @@ export default defineComponent({
   mounted() {
     if (!this.isWebGLAvailable) return
 
+    initWorld()
+
     let threeAppDiv = document.getElementById('three-app')
     this.onResize()
     threeAppDiv.appendChild(renderer.domElement)
@@ -48,10 +112,6 @@ export default defineComponent({
 
     controls.addEventListener('start', () => this.isGrabbing = true)
     controls.addEventListener('end', () => this.isGrabbing = false)
-
-    scene.add(cube)
-
-    camera.position.z = 5
 
     this.animate()
   },
@@ -81,9 +141,6 @@ export default defineComponent({
       controls.update()
 
       renderer.render(scene, camera)
-
-      cube.rotation.x += 0.01
-      cube.rotation.y += 0.01
     }
   }
 })

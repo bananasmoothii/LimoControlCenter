a4bf7eaf4d9145c843531c82dcecf74d72583ea9
wassimml <mlaouhiawpro@gmail.com>
2024-03-28 18:18:03 +0100

    changed initial coords

--- KotlinServer/src/main/kotlin/robots/Robot.kt
+++ KotlinServer/src/main/kotlin/robots/Robot.kt
@@ -1,6 +1,6 @@
 package fr.bananasmoothii.limocontrolcenter.robots
 
 class Robot(val id: String) {
-    var lastPosString: String = "0,0,0"
+    var lastPosString: String = "2.57,2.55,0"
     var lastReceivedKeepAlive: Long = 0
 }

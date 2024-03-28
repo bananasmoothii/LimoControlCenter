98b61352a25a2a57345b42b32d44cc823b6db1af
wassimml <mlaouhiawpro@gmail.com>
2024-03-28 20:33:23 +0100

    launch file

---
+++ Python_limo/launch.sh
@@ -0,0 +1,5 @@
+tmux new-session -d -s limo_base "roslaunch limo_base limo_base.launch"
+sleep 5
+tmux new-session -d -s limo_bringup_start "roslaunch limo_bringup limo_start.launch"
+sleep 5
+tmux new-session -d -s limo_bringup_nav "roslaunch limo_bringup limo_navigation_ackerman.launch"
